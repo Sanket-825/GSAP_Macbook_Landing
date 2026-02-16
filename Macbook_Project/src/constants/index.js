2d9ef28198688e264dda7b2df9c79a4e485851bc
Sanket Parab <sanketparab59@gmail.com>
2026-02-17 01:55:58 +0530

    first commit

---
+++ Macbook_Project/src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+    { label: "Store", path: "/store" },
+    { label: "Mac", path: "/mac" },
+    { label: "iPhone", path: "/iphone" },
+    { label: "Watch", path: "/watch" },
+    { label: "Vision", path: "/vision" },
+    { label: "Airpods", path: "/airpods" },
+];
