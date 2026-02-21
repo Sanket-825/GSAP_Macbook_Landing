214e2626699f0f5baa7424080913bb5039214338
Sanket Parab <sanketparab59@gmail.com>
2026-02-21 15:47:20 +0530

    Implement Footer

--- Macbook_Project/src/components/Footer.jsx
+++ Macbook_Project/src/components/Footer.jsx
@@ -1,7 +1,27 @@
+import { footerLinks } from "../constants"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="info">
+        <p>More ways to shop: <a href="https://www.apple.com/in/retail" className="text-blue-500 underline" target="_blank">Find an Apple Store</a> or <a href="https://locate.apple.com" className="text-blue-500 underline" target="_blank">other retailer </a> near you. Or call 000800 040 1966</p>
+        <img src="/logo.svg" alt="Apple Logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2026 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({label, link})=>(
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
